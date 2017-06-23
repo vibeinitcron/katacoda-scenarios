8c760972cef40859ffbb8b935453d6dfdd5ebefb
vibeinitcron <vibe@initcron.org>
2017-06-23 18:15:35 +0530

    background puppet installation

--- puppet/launch.sh
+++ puppet/launch.sh
@@ -1,8 +1,10 @@
 cd /tmp
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-sudo dpkg -i puppetlabs-release-trusty.deb
-sudo apt-get update
-sudo apt-get install puppet -y
+echo "Installing Puppet...."
+wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb > /dev/null 2>&1
+sudo dpkg -i puppetlabs-release-trusty.deb > /dev/null 2>&1
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get install puppet -y > /dev/null 2>&1
 cd /home/ubuntu
 git clone https://github.com/vibeinitcron/manifest-katacoda.git
 cd manifest-katacoda
+echo "DONE "
