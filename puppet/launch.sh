e0f4bb0cda54f65d6d6c64f0c03f7cba9117bd01
vibeinitcron <vibe@initcron.org>
2017-06-23 17:35:30 +0530

    code

--- puppet/launch.sh
+++ puppet/launch.sh
@@ -3,6 +3,6 @@ wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
 sudo apt-get update
 sudo apt-get install puppet
-cd /home/scrapbook/tutorial/manifest-katacoda
+cd /home/scrapbook/tutorial/
 git clone https://github.com/vibeinitcron/manifest-katacoda.git
 cd manifest-katacoda
