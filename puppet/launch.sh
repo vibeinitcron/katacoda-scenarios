0aa503ae579233e4a74b2d0e62d63f0b661fb5d4
vibeinitcron <vibe@initcron.org>
2017-06-23 17:27:16 +0530

    code

--- puppet/launch.sh
+++ puppet/launch.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/vibeinitcron/manifest-katacoda.git
 cd manifest-katacoda
-docker run -itd --name puppet --hostname puppet -v /home/scrapbook/tutorial/manifest-katacoda/code:/code puppet/puppetserver
+docker run -itd --name puppet --hostname puppet -v /home/scrapbook/tutorial/manifest-katacoda/code:/etc/puppetlabs/code/ puppet/puppetserver
 docker exec -it puppet bash
